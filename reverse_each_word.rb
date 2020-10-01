e41e1d2a38175f8717ce2b21c4f1989596549bfa
Roberto Acosta <x.rojo@hotmail.com>
2020-10-01 03:09:28 +0000

    Automatically backed up by Learn

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -1,10 +1,4 @@
 def reverse_each_word(phrase)
-  # word_array=phrase.split (" ")
-  # new_array=[]
-  # word_array.each do |word|
-  #   new_array << word.reverse
-  # end
-  #new_array.join(" ")
-  phrase.split(" ").collect {|x| x.reverse}.join(" ")
+   phrase.split(" ").collect {|x| x.reverse}.join(" ")
 end
 
